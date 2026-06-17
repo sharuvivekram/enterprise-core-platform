837053e98946660bab7cbea558f36d450c391c86
unknown <sharuvivekram@gmail.com>
2026-06-17 13:28:10 +0530

    infra: connect aws cli and instantiate root terraform configurations for production vpc

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Call your custom VPC Module
+module "production_vpc" {
+  source               = "./modules/vpc"
+  vpc_cidr             = "10.0.0.0/16"
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
+  environment          = "Production"
+}
